a336f1daa1f77cef8e6e1db1a556d4b8fa682d6d
dmswl <76807107+dmswl98@users.noreply.github.com>
2022-08-07 12:49:51 +0900

    🚧 Register the routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Result from "./pages/Result";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/result" element={<Result />} />
+    </Routes>
+  );
 }
 
 export default App;
