729db527eced30195c52c2ba05c7a2c05c227145
dmswl <76807107+dmswl98@users.noreply.github.com>
2022-08-08 15:07:05 +0900

    🚧 Create redux-search for user's search word and Set up redux store

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import resultSlice from "./result-slice";
+import searchSlice from "./search-slice";
 
 const store = configureStore({
   reducer: {
+    search: searchSlice.reducer,
     result: resultSlice.reducer,
   },
 });
--- src/store/search-slice.js
+++ src/store/search-slice.js
@@ -1,29 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const searchSlice = createSlice({
-  name: "search-result",
+  name: "search",
   initialState: {
-    result: {
-      id: "",
-      word: "",
-      meanings: [],
-      phonetics: [],
-    },
-    isLoading: false,
-    errorMessage: "",
+    word: "",
   },
   reducers: {
-    loadData(state, action) {
-      state.isLoading = action.payload;
-    },
-    replaceWord(state, action) {
-      state.result.id = action.payload.id;
-      state.result.word = action.payload.word;
-      state.result.meanings = action.payload.meanings;
-      state.result.phonetics = action.payload.phonetics;
-    },
-    occurError(state, action) {
-      state.errorMessage = action.payload;
+    searchWord(state, action) {
+      state.word = action.payload;
     },
   },
 });
