82d3576e1637150a02d24629f10f0544eed7a6d1
dmswl <76807107+dmswl98@users.noreply.github.com>
2022-08-09 17:17:59 +0900

    🚧 Remove home page and set nested routing of result page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
 import Result from "./pages/Result";
+import ResultDetail from "./pages/ResultDetail";
+import MyPage from "./pages/MyPage";
 import "./App.css";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/result" element={<Result />} />
+      <Route path="/" element={<Result />}>
+        <Route path=":word" element={<ResultDetail />} />
+      </Route>
+      <Route path="/mypage" element={<MyPage />} />
+      <Route path="*" element={<p>404</p>} />
     </Routes>
   );
 }
