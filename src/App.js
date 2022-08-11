c1aed1bc2a793120064e02b05ad6db9aec20d3d1
dmswl <76807107+dmswl98@users.noreply.github.com>
2022-08-11 23:17:53 +0900

    🚧 Register route of wordlist page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Result from "./pages/Result";
 import ResultDetail from "./pages/ResultDetail";
 import MyPage from "./pages/MyPage";
+import WordList from "./pages/WordList";
 import "./App.css";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path=":word" element={<ResultDetail />} />
       </Route>
       <Route path="/mypage" element={<MyPage />} />
+      <Route path="/mypage/:folder" element={<WordList />} />
       <Route path="*" element={<p>404</p>} />
     </Routes>
   );
