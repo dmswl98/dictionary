14e18e34c002cab0e3146819d31b60627c01e123
dmswl <76807107+dmswl98@users.noreply.github.com>
2022-08-13 13:21:19 +0900

    🩹 Change the position of a button tag

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import Layout from "../components/Layout/Layout";
-import WordList from "../components/Word/WordList";
 import NoWordsFound from "../components/Word/NoWordsFound";
 import classes from "./MyPage.module.css";
 import FolderModal from "../components/Word/FolderModal";
@@ -26,11 +25,11 @@ const MyPage = () => {
   return (
     <Layout>
       <div className={classes["my-page"]}>
+        {folderModalIsShown && <FolderModal onClose={hideModalHandler} />}
+        {folders.length === 0 && <NoWordsFound onShow={showModalHandler} />}
         <button className={classes.button} onClick={showModalHandler}>
           Create a new list
         </button>
-        {folderModalIsShown && <FolderModal onClose={hideModalHandler} />}
-        {folders.length === 0 && <NoWordsFound onShow={showModalHandler} />}
         {folders.length !== 0 && <FolderList folders={folders} />}
         {/* {items.length !== 0 && <WordList items={items} />} */}
       </div>
